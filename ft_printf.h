19396963e0ac0b22b8f7d7821dda18f8546e0012
Kerem Apaydın <apaydinkerem18@gmail.com>
2026-03-20 13:02:36 +0300

    Update: ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kapaydin <kapaydin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 21:30:00 by kapaydin          #+#    #+#             */
-/*   Updated: 2026/03/19 21:53:01 by kapaydin         ###   ########.fr       */
+/*   Updated: 2026/03/20 13:01:13 by kapaydin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 
+int	ft_printf(const char *format, ...);
 int	ft_print_char(int c);
-int	ft_print_str(char *s);
+int	ft_print_hex(unsigned long long n, char format);
 int	ft_print_nbr(long n);
-int	ft_print_hex(unsigned int n, char format);
+int	ft_print_ptr(unsigned long long ptr);
+int	ft_print_str(char *s);
 
 #endif
